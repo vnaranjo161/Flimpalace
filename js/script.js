70f0129b093e98c140d374f09837636f9b1c1e67
Valentina Naranjo Mejia <vnaranjo161@gmail.com>
2023-08-16 19:20:21 -0500

    Responsive y modal

--- js/script.js
+++ js/script.js
@@ -13,17 +13,50 @@ document.addEventListener('keyup', e => {
     }
 });
 
-document.addEventListener('keyup', e => {
-    if (e.target.matches('#buscadorCategorias')) {
-        const searchTerm = e.target.value.toLowerCase();
-        document.querySelectorAll('.peliculasYSeries').forEach(pelicula => {
-           const titulo = pelicula.querySelector('.genero').textContent.toLowerCase();
-           if (titulo.includes(searchTerm)) {
-            pelicula.classList.remove("filtro");
-           } else {
-            pelicula.classList.add("filtro");
-           }
 
-        });
-    }
+
+//----------------------------------------------------
+const modal = document.getElementById('myModal');
+const modalBtns = document.querySelectorAll('.modal-btn');
+const closeBtn = document.getElementById('closeBtn');
+const videoFrame = document.getElementById('videoFrame');
+
+// Abrir ventana modal y cargar video
+modalBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const videoId = btn.getAttribute('data-video');
+    const videoUrl = `https://www.youtube.com/embed/${videoId}`;
+    videoFrame.setAttribute('src', videoUrl);
+    modal.style.display = 'block';
+  });
+});
+
+// Cerrar ventana modal y detener el video
+closeBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+  videoFrame.setAttribute('src', '');
+});
+
+// Cerrar ventana modal haciendo clic fuera del contenido
+window.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+    videoFrame.setAttribute('src', '');
+  }
+});
+
+
+document.addEventListener('keyup', e => {
+  if (e.target.matches('#buscadorCategorias')) {
+      const searchTerm = e.target.value.toLowerCase();
+      document.querySelectorAll('.peliculasYSeries').forEach(pelicula => {
+         const titulo = pelicula.querySelector('.genero').textContent.toLowerCase();
+         if (titulo.includes(searchTerm)) {
+          pelicula.classList.remove("filtro");
+         } else {
+          pelicula.classList.add("filtro");
+         }
+
+      });
+  }
 });
